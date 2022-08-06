de38dc1b9b375e01f60bc760d7084c05e802961a
Jochen Greiner <jochen@greiner.de>
2022-08-06 16:30:50 +0200

    add bs package

--- packages/site_package/Classes/Controller/DemoController.php
+++ packages/site_package/Classes/Controller/DemoController.php
@@ -11,14 +11,12 @@ class DemoController extends ActionController
 {
     public function demoAction(): JsonResponse
     {
-<<<<<<< HEAD
+
         $data = [];
-        for($i=0;$i<=50;$i++) {
-            $data['item'.$i] = random_int(0,10000);
+        for ($i = 0; $i <= 50; $i++) {
+            $data['item' . $i] = random_int(0, 10000);
         }
         return new JsonResponse($data);
-=======
-        return new JsonResponse(['item' => 'test']);
->>>>>>> a82f0f1f505022784ff34e24d0d4cc2040813f14
+
     }
 }
